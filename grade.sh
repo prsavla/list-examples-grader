08fefd1f810d77aeece1b0332bb85a108d20cb78
prsavla <prchheda@ucsd.edu>
2024-03-07 14:04:07 -0800

    Finished AutoGrader - Pratham

--- grade.sh
+++ grade.sh
@@ -14,3 +14,53 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+# Check that the file exits
+if [ -f "student-submission/ListExamples.java" ]; then 
+    echo "File Found!"
+else 
+    echo "ListExamples.java not found!"
+    exit 1
+fi
+
+#jars
+cp -r lib grading-area
+
+#List Examples
+cp student-submission/ListExamples.java grading-area/
+
+#TestListExamples
+cp TestListExamples.java grading-area/
+
+cd grading-area
+javac -cp $CPATH *.java > outputOfJavaC.txt 2>&1
+
+if [ $? -ne 0 ]; then
+  echo "Won't Compile"
+  exit 1
+else
+    echo "Compiled succesfully!"
+fi
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > jUnitOutput.txt
+
+last_line=$(tail -n 3 jUnitOutput.txt)
+if [[ $last_line =~ "OK" ]]; then
+    echo "Grade: 100%"
+    exit 0
+fi
+
+tests_run=$(echo "$last_line" | grep -o 'Tests run: [0-9]*' | awk '{print $3}')
+failures=$(echo "$last_line" | grep -o 'Failures: [0-9]*' | awk '{print $2}')
+
+correct=$((tests_run - failures))
+echo "The amount correct is" 
+echo $correct
+echo "The amount of tests run is"
+echo $tests_run
+echo "Your grade is"
+percentage=$(echo "scale=2; $correct / $tests_run * 100" | bc)
+echo $percentage "%"
+
+
+
+
